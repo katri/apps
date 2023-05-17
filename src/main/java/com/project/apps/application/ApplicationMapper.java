a82ee6ed717a46680a587d845678677cce8d0026
Katri Pokats <katri.pokats@gmail.com>
2023-05-17 15:41:55 +0300

    Cleanup

--- src/main/java/com/project/apps/application/ApplicationMapper.java
+++ src/main/java/com/project/apps/application/ApplicationMapper.java
@@ -1,19 +1,13 @@
 package com.project.apps.application;
 
-import com.project.apps.appRoutine.AppRoutine;
 import org.mapstruct.*;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface ApplicationMapper {
     Application toEntity(ApplicationRequestView applicationRequestView);
 
-    ApplicationRequestView toDto(Application application);
-
     ApplicationResponseView toResponseDto(Application application);
     @Mapping(target = "routineName", source = "appRoutineName")
-//    @Mapping(target = "id", source = "application.id")
-//    @Mapping(target = "name", source = "application.name")
-//    @Mapping(target = "description", source = "application.description")
     ApplicationResponseWithRoutineView toResponseWithRoutineInfoDto(Application application, String appRoutineName);
 
     @Mapping(target = "appId", source = "id")
